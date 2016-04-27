52467a005d944a5e73610eb5f10e08136b545a5d
alannaiverson <alannaiverson@gmail.com>
2016-04-27 15:52:19 -0700

    Added loader functions

--- doc/source/report/plots/heat_map_0_0_0.py
+++ doc/source/report/plots/heat_map_0_0_0.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 from mousestyles.data.utils import day_to_mouse_average, mouse_to_strain_average, total_time_rectangle_bins, pull_locom_tseries_subset, split_data_in_half_randomly
 
 # Make position density first mouse first day
-# from mousestyles.data import load_movement
-# #maybe M = load_movement(0, 0, 0)
-CT = np.load('../../../../mousestyles/data/txy_coords/CT/CT_strain0_mouse0_day0.npy')
-CX = np.load('../../../../mousestyles/data/txy_coords/CX/CX_strain0_mouse0_day0.npy')
-CY = np.load('../../../../mousestyles/data/txy_coords/CY/CY_strain0_mouse0_day0.npy')
+from mousestyles.data import load_movement,load_start_time_end_time
+M = load_movement(0, 0, 0)
+CT = M['t']
+CX = M['x']
+CY = M['y']
 
 # mask for HB Move Events
-CT_NHB = np.load('../../../../mousestyles/data/txy_coords/C_idx_HB/C_idx_HB_strain0_mouse0_day0.npy')
+CT_NHB = M['isHB']
 CT_HB = ~ CT_NHB
 
-start_time, stop_time = np.load('../../../../mousestyles/data/txy_coords/recordingStartTimeEndTime/recordingStartTimeEndTime_strain0_mouse0_day0.npy')
+start_time, stop_time = load_start_time_end_time(0,0,0)
 
 # Cage boundaries
 YLower = 1.0; YUpper = 43.0; XUpper = 3.75; XLower = -16.25
